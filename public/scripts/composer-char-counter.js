6eb017f8cdd3faac8a7d312101dabec6b837a523
puneet2121 <bedipuneet21@gmail.com>
2022-05-25 22:35:15 +0000

    adding all the file to repo made a tweeter page with css html and jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function(){ 
+    let text =  $(this).val()
+    let len = text.length;
+    let char = 140 - len;
+    if (len >= 140) {
+      $('#count').text(char);
+      $('#count').css('color','red');
+    } else {
+      $('#count').css('color','#545149');
+      $('#count').text(char);
+      
+    }
+  });
+    
+});
+
