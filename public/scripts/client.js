53d276acc14d451955806c92daa5ddb96827858b
puneet2121 <bedipuneet21@gmail.com>
2022-05-26 05:32:05 +0000

    adding timeago to my tweeter project getting data from the server and showing it on my app

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1653348097881
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1653434497882
-  }
-]
+
 
 const createTweetElement = function(tweet) {
   console.log(tweet)
@@ -37,7 +14,7 @@ const createTweetElement = function(tweet) {
 
         </form>
         <hr class="article-line">
-      <footer><div>10 days ago</div>
+      <footer><div>${timeago.format(tweet.created_at)};</div>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -47,16 +24,19 @@ const createTweetElement = function(tweet) {
   </article>`
   $('#tweets-container').append(tweetHtml);
 }
+ 
 
 const renderTweets = function() {
-    for(let items of tweetData) {
+  $.get('/tweets',(res) => {
+    for(let items of res) {
       createTweetElement(items);
     }
+})
 }
+
 $(() =>  {
 renderTweets();
 $("form").submit(function(event) {
-  alert("an event has occured");
   event.preventDefault();
   let serializeData = $( this ).serialize() 
   $.post('/tweets',serializeData,() => {
